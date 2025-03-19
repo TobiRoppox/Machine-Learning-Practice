1d2a2580f23893abfc6d0b5136e79ead40853379
Clifford Halasan <cliffordhalasan11@gmail.com>
2025-03-19 12:12:28 -0700

    Linear Regression

--- test.py
+++ test.py
@@ -14,4 +14,18 @@ predict = "G3"
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict ])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc= linear.score(x_test, y_test)
+print(acc)
+
+print("Coefficient: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
